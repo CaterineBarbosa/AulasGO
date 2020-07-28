5f150f3df7dd0ee19a7a56e9ec53ae8c7e55946d
Caterine <Caterine.barbosa@live.com>
2020-07-27 22:06:52 -0300

    Publicação Olá Planeta!

--- hello.go
+++ hello.go
@@ -3,18 +3,5 @@ package main
 import "fmt"
 
 func main(){
-	var num1 = 20
-	var num2 = 10
-	fmt.Println("Minha calculadora de somar ")
-	Calculadora(num1,num2)
+	fmt.Println("Olá Planeta!!")
 }
-func Calculadora(a int,b int){
-	var result= a+b
-	fmt.Println(result)
-}
-
-//Fazer as quatro operações 
-//teste
-
-
-
