686930542945f22d3c9ea13a8a825641db1f786f
Caterine <Caterine.barbosa@live.com>
2020-07-27 21:21:41 -0300

    primeiro pacote

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	var num1 = 20
+	var num2 = 10
+	fmt.Println("Minha calculadora de somar ")
+	Calculadora(num1,num2)
+}
+func Calculadora(a int,b int){
+	var result= a+b
+	fmt.Println(result)
+}
+
+//Fazer as quatro operações 
+
+
